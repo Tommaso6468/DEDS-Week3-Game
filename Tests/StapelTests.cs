01396db0a3be581486f83ddaec7d1a8a8dc8bcbc
Tommaso van Gelderen <tommasovangelderen@gmail.com>
2023-02-20 16:31:07 +0100

    tests added

--- Tests/StapelTests.cs
+++ Tests/StapelTests.cs
@@ -3,10 +3,10 @@ namespace Tests;
 using Main;
 using Xunit;
 
-public class StapelTests<T>
+public class StapelTests
 {
     [Fact]
-    public void StapelDuwEnPak()
+    public void StapelDuwEnPakEnkel()
     {
         var stapel = new Stapel<int>();
         var cijfer = 1;
@@ -15,5 +15,36 @@ public class StapelTests<T>
         var resultaatPak = stapel.Pak();
 
         Assert.Equal(resultaatPak, cijfer);
+    }
+
+    [Fact]
+    public void StapelDuwEnPakMeerdere()
+    {
+        var stapel = new Stapel<string>();
+        string[] strings = { "a", "b", "c" };
+        var verwachtResultaat = new List<string>();
+        verwachtResultaat.Add(strings[2]);
+        verwachtResultaat.Add(strings[1]);
+        verwachtResultaat.Add(strings[0]);
+
+        stapel.Duw(ref strings[0]);
+        stapel.Duw(ref strings[1]);
+        stapel.Duw(ref strings[2]);
+
+        var resultaatPak = new List<string>();
+        resultaatPak.Add(stapel.Pak());
+        resultaatPak.Add(stapel.Pak());
+        resultaatPak.Add(stapel.Pak());
+
+        Assert.Equal(resultaatPak, verwachtResultaat);
+    }
+
+    [Fact]
+    public void StapelPakLeeg()
+    {
+        var stapel = new Stapel<int>();
+        var resultaatPak = stapel.Pak();
+
+        Assert.Equal(resultaatPak, default);
     }
 }
